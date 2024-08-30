30a1ea8ec52552d8158053f011fc0226ab7085e7
Richard Dewan <richarddewan@hotmail.com>
2024-08-30 18:15:50 +0700

    included generated code .freezed and .g.dart file in git

---
+++ packages/account/lib/presentation/state/account_state.freezed.dart
@@ -0,0 +1,382 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'account_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$AccountState {
+  bool get isLoading => throw _privateConstructorUsedError;
+  bool get isUpdating => throw _privateConstructorUsedError;
+  bool get isDeleteting => throw _privateConstructorUsedError;
+  bool get isSigningOut => throw _privateConstructorUsedError;
+  Map<String, dynamic> get updateMeFormData =>
+      throw _privateConstructorUsedError;
+  String? get profilePhotoUrl => throw _privateConstructorUsedError;
+  bool? get isDeleteMe => throw _privateConstructorUsedError;
+  bool? get isSignOut => throw _privateConstructorUsedError;
+  UserEntity? get user => throw _privateConstructorUsedError;
+  int? get statusCode => throw _privateConstructorUsedError;
+  String? get errorMsg => throw _privateConstructorUsedError;
+
+  /// Create a copy of AccountState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $AccountStateCopyWith<AccountState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AccountStateCopyWith<$Res> {
+  factory $AccountStateCopyWith(
+          AccountState value, $Res Function(AccountState) then) =
+      _$AccountStateCopyWithImpl<$Res, AccountState>;
+  @useResult
+  $Res call(
+      {bool isLoading,
+      bool isUpdating,
+      bool isDeleteting,
+      bool isSigningOut,
+      Map<String, dynamic> updateMeFormData,
+      String? profilePhotoUrl,
+      bool? isDeleteMe,
+      bool? isSignOut,
+      UserEntity? user,
+      int? statusCode,
+      String? errorMsg});
+}
+
+/// @nodoc
+class _$AccountStateCopyWithImpl<$Res, $Val extends AccountState>
+    implements $AccountStateCopyWith<$Res> {
+  _$AccountStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of AccountState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isLoading = null,
+    Object? isUpdating = null,
+    Object? isDeleteting = null,
+    Object? isSigningOut = null,
+    Object? updateMeFormData = null,
+    Object? profilePhotoUrl = freezed,
+    Object? isDeleteMe = freezed,
+    Object? isSignOut = freezed,
+    Object? user = freezed,
+    Object? statusCode = freezed,
+    Object? errorMsg = freezed,
+  }) {
+    return _then(_value.copyWith(
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isUpdating: null == isUpdating
+          ? _value.isUpdating
+          : isUpdating // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isDeleteting: null == isDeleteting
+          ? _value.isDeleteting
+          : isDeleteting // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSigningOut: null == isSigningOut
+          ? _value.isSigningOut
+          : isSigningOut // ignore: cast_nullable_to_non_nullable
+              as bool,
+      updateMeFormData: null == updateMeFormData
+          ? _value.updateMeFormData
+          : updateMeFormData // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      profilePhotoUrl: freezed == profilePhotoUrl
+          ? _value.profilePhotoUrl
+          : profilePhotoUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      isDeleteMe: freezed == isDeleteMe
+          ? _value.isDeleteMe
+          : isDeleteMe // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      isSignOut: freezed == isSignOut
+          ? _value.isSignOut
+          : isSignOut // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      user: freezed == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as UserEntity?,
+      statusCode: freezed == statusCode
+          ? _value.statusCode
+          : statusCode // ignore: cast_nullable_to_non_nullable
+              as int?,
+      errorMsg: freezed == errorMsg
+          ? _value.errorMsg
+          : errorMsg // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$AccountStateImplCopyWith<$Res>
+    implements $AccountStateCopyWith<$Res> {
+  factory _$$AccountStateImplCopyWith(
+          _$AccountStateImpl value, $Res Function(_$AccountStateImpl) then) =
+      __$$AccountStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {bool isLoading,
+      bool isUpdating,
+      bool isDeleteting,
+      bool isSigningOut,
+      Map<String, dynamic> updateMeFormData,
+      String? profilePhotoUrl,
+      bool? isDeleteMe,
+      bool? isSignOut,
+      UserEntity? user,
+      int? statusCode,
+      String? errorMsg});
+}
+
+/// @nodoc
+class __$$AccountStateImplCopyWithImpl<$Res>
+    extends _$AccountStateCopyWithImpl<$Res, _$AccountStateImpl>
+    implements _$$AccountStateImplCopyWith<$Res> {
+  __$$AccountStateImplCopyWithImpl(
+      _$AccountStateImpl _value, $Res Function(_$AccountStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of AccountState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? isLoading = null,
+    Object? isUpdating = null,
+    Object? isDeleteting = null,
+    Object? isSigningOut = null,
+    Object? updateMeFormData = null,
+    Object? profilePhotoUrl = freezed,
+    Object? isDeleteMe = freezed,
+    Object? isSignOut = freezed,
+    Object? user = freezed,
+    Object? statusCode = freezed,
+    Object? errorMsg = freezed,
+  }) {
+    return _then(_$AccountStateImpl(
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isUpdating: null == isUpdating
+          ? _value.isUpdating
+          : isUpdating // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isDeleteting: null == isDeleteting
+          ? _value.isDeleteting
+          : isDeleteting // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSigningOut: null == isSigningOut
+          ? _value.isSigningOut
+          : isSigningOut // ignore: cast_nullable_to_non_nullable
+              as bool,
+      updateMeFormData: null == updateMeFormData
+          ? _value._updateMeFormData
+          : updateMeFormData // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      profilePhotoUrl: freezed == profilePhotoUrl
+          ? _value.profilePhotoUrl
+          : profilePhotoUrl // ignore: cast_nullable_to_non_nullable
+              as String?,
+      isDeleteMe: freezed == isDeleteMe
+          ? _value.isDeleteMe
+          : isDeleteMe // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      isSignOut: freezed == isSignOut
+          ? _value.isSignOut
+          : isSignOut // ignore: cast_nullable_to_non_nullable
+              as bool?,
+      user: freezed == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as UserEntity?,
+      statusCode: freezed == statusCode
+          ? _value.statusCode
+          : statusCode // ignore: cast_nullable_to_non_nullable
+              as int?,
+      errorMsg: freezed == errorMsg
+          ? _value.errorMsg
+          : errorMsg // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$AccountStateImpl implements _AccountState {
+  _$AccountStateImpl(
+      {this.isLoading = false,
+      this.isUpdating = false,
+      this.isDeleteting = false,
+      this.isSigningOut = false,
+      final Map<String, dynamic> updateMeFormData = const {},
+      this.profilePhotoUrl,
+      this.isDeleteMe,
+      this.isSignOut,
+      this.user,
+      this.statusCode,
+      this.errorMsg})
+      : _updateMeFormData = updateMeFormData;
+
+  @override
+  @JsonKey()
+  final bool isLoading;
+  @override
+  @JsonKey()
+  final bool isUpdating;
+  @override
+  @JsonKey()
+  final bool isDeleteting;
+  @override
+  @JsonKey()
+  final bool isSigningOut;
+  final Map<String, dynamic> _updateMeFormData;
+  @override
+  @JsonKey()
+  Map<String, dynamic> get updateMeFormData {
+    if (_updateMeFormData is EqualUnmodifiableMapView) return _updateMeFormData;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableMapView(_updateMeFormData);
+  }
+
+  @override
+  final String? profilePhotoUrl;
+  @override
+  final bool? isDeleteMe;
+  @override
+  final bool? isSignOut;
+  @override
+  final UserEntity? user;
+  @override
+  final int? statusCode;
+  @override
+  final String? errorMsg;
+
+  @override
+  String toString() {
+    return 'AccountState(isLoading: $isLoading, isUpdating: $isUpdating, isDeleteting: $isDeleteting, isSigningOut: $isSigningOut, updateMeFormData: $updateMeFormData, profilePhotoUrl: $profilePhotoUrl, isDeleteMe: $isDeleteMe, isSignOut: $isSignOut, user: $user, statusCode: $statusCode, errorMsg: $errorMsg)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AccountStateImpl &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            (identical(other.isUpdating, isUpdating) ||
+                other.isUpdating == isUpdating) &&
+            (identical(other.isDeleteting, isDeleteting) ||
+                other.isDeleteting == isDeleteting) &&
+            (identical(other.isSigningOut, isSigningOut) ||
+                other.isSigningOut == isSigningOut) &&
+            const DeepCollectionEquality()
+                .equals(other._updateMeFormData, _updateMeFormData) &&
+            (identical(other.profilePhotoUrl, profilePhotoUrl) ||
+                other.profilePhotoUrl == profilePhotoUrl) &&
+            (identical(other.isDeleteMe, isDeleteMe) ||
+                other.isDeleteMe == isDeleteMe) &&
+            (identical(other.isSignOut, isSignOut) ||
+                other.isSignOut == isSignOut) &&
+            (identical(other.user, user) || other.user == user) &&
+            (identical(other.statusCode, statusCode) ||
+                other.statusCode == statusCode) &&
+            (identical(other.errorMsg, errorMsg) ||
+                other.errorMsg == errorMsg));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      isLoading,
+      isUpdating,
+      isDeleteting,
+      isSigningOut,
+      const DeepCollectionEquality().hash(_updateMeFormData),
+      profilePhotoUrl,
+      isDeleteMe,
+      isSignOut,
+      user,
+      statusCode,
+      errorMsg);
+
+  /// Create a copy of AccountState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$AccountStateImplCopyWith<_$AccountStateImpl> get copyWith =>
+      __$$AccountStateImplCopyWithImpl<_$AccountStateImpl>(this, _$identity);
+}
+
+abstract class _AccountState implements AccountState {
+  factory _AccountState(
+      {final bool isLoading,
+      final bool isUpdating,
+      final bool isDeleteting,
+      final bool isSigningOut,
+      final Map<String, dynamic> updateMeFormData,
+      final String? profilePhotoUrl,
+      final bool? isDeleteMe,
+      final bool? isSignOut,
+      final UserEntity? user,
+      final int? statusCode,
+      final String? errorMsg}) = _$AccountStateImpl;
+
+  @override
+  bool get isLoading;
+  @override
+  bool get isUpdating;
+  @override
+  bool get isDeleteting;
+  @override
+  bool get isSigningOut;
+  @override
+  Map<String, dynamic> get updateMeFormData;
+  @override
+  String? get profilePhotoUrl;
+  @override
+  bool? get isDeleteMe;
+  @override
+  bool? get isSignOut;
+  @override
+  UserEntity? get user;
+  @override
+  int? get statusCode;
+  @override
+  String? get errorMsg;
+
+  /// Create a copy of AccountState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$AccountStateImplCopyWith<_$AccountStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ packages/member/lib/domain/member_model.freezed.dart
@@ -0,0 +1,260 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'member_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$MemberModel {
+  int get id => throw _privateConstructorUsedError;
+  String get name => throw _privateConstructorUsedError;
+  String get email => throw _privateConstructorUsedError;
+  String get phoneNumber => throw _privateConstructorUsedError;
+  String? get photo => throw _privateConstructorUsedError;
+  List<String> get roles => throw _privateConstructorUsedError;
+
+  /// Create a copy of MemberModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $MemberModelCopyWith<MemberModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MemberModelCopyWith<$Res> {
+  factory $MemberModelCopyWith(
+          MemberModel value, $Res Function(MemberModel) then) =
+      _$MemberModelCopyWithImpl<$Res, MemberModel>;
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String email,
+      String phoneNumber,
+      String? photo,
+      List<String> roles});
+}
+
+/// @nodoc
+class _$MemberModelCopyWithImpl<$Res, $Val extends MemberModel>
+    implements $MemberModelCopyWith<$Res> {
+  _$MemberModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of MemberModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? email = null,
+    Object? phoneNumber = null,
+    Object? photo = freezed,
+    Object? roles = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phoneNumber: null == phoneNumber
+          ? _value.phoneNumber
+          : phoneNumber // ignore: cast_nullable_to_non_nullable
+              as String,
+      photo: freezed == photo
+          ? _value.photo
+          : photo // ignore: cast_nullable_to_non_nullable
+              as String?,
+      roles: null == roles
+          ? _value.roles
+          : roles // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$MemberModelImplCopyWith<$Res>
+    implements $MemberModelCopyWith<$Res> {
+  factory _$$MemberModelImplCopyWith(
+          _$MemberModelImpl value, $Res Function(_$MemberModelImpl) then) =
+      __$$MemberModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      String name,
+      String email,
+      String phoneNumber,
+      String? photo,
+      List<String> roles});
+}
+
+/// @nodoc
+class __$$MemberModelImplCopyWithImpl<$Res>
+    extends _$MemberModelCopyWithImpl<$Res, _$MemberModelImpl>
+    implements _$$MemberModelImplCopyWith<$Res> {
+  __$$MemberModelImplCopyWithImpl(
+      _$MemberModelImpl _value, $Res Function(_$MemberModelImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of MemberModel
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? email = null,
+    Object? phoneNumber = null,
+    Object? photo = freezed,
+    Object? roles = null,
+  }) {
+    return _then(_$MemberModelImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phoneNumber: null == phoneNumber
+          ? _value.phoneNumber
+          : phoneNumber // ignore: cast_nullable_to_non_nullable
+              as String,
+      photo: freezed == photo
+          ? _value.photo
+          : photo // ignore: cast_nullable_to_non_nullable
+              as String?,
+      roles: null == roles
+          ? _value._roles
+          : roles // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$MemberModelImpl implements _MemberModel {
+  _$MemberModelImpl(
+      {this.id = 0,
+      this.name = '',
+      this.email = '',
+      this.phoneNumber = '',
+      this.photo,
+      final List<String> roles = const []})
+      : _roles = roles;
+
+  @override
+  @JsonKey()
+  final int id;
+  @override
+  @JsonKey()
+  final String name;
+  @override
+  @JsonKey()
+  final String email;
+  @override
+  @JsonKey()
+  final String phoneNumber;
+  @override
+  final String? photo;
+  final List<String> _roles;
+  @override
+  @JsonKey()
+  List<String> get roles {
+    if (_roles is EqualUnmodifiableListView) return _roles;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_roles);
+  }
+
+  @override
+  String toString() {
+    return 'MemberModel(id: $id, name: $name, email: $email, phoneNumber: $phoneNumber, photo: $photo, roles: $roles)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$MemberModelImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.phoneNumber, phoneNumber) ||
+                other.phoneNumber == phoneNumber) &&
+            (identical(other.photo, photo) || other.photo == photo) &&
+            const DeepCollectionEquality().equals(other._roles, _roles));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, email, phoneNumber,
+      photo, const DeepCollectionEquality().hash(_roles));
+
+  /// Create a copy of MemberModel
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$MemberModelImplCopyWith<_$MemberModelImpl> get copyWith =>
+      __$$MemberModelImplCopyWithImpl<_$MemberModelImpl>(this, _$identity);
+}
+
+abstract class _MemberModel implements MemberModel {
+  factory _MemberModel(
+      {final int id,
+      final String name,
+      final String email,
+      final String phoneNumber,
+      final String? photo,
+      final List<String> roles}) = _$MemberModelImpl;
+
+  @override
+  int get id;
+  @override
+  String get name;
+  @override
+  String get email;
+  @override
+  String get phoneNumber;
+  @override
+  String? get photo;
+  @override
+  List<String> get roles;
+
+  /// Create a copy of MemberModel
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$MemberModelImplCopyWith<_$MemberModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
