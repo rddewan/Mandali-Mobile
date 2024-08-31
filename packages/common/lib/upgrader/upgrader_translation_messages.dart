d8f9d43cb8f22f17cb0ddcb010a2883eabbc9376
R Dewan <13164167+rddewan@users.noreply.github.com>
2024-08-31 18:16:31 +0700

    update codemagic script (#4)
    * update codemagic build script
    
    * add build_runner to codemagic script
    
    * update run_build_runner.sh script
    
    * included generated code .freezed and .g.dart file in git
    
    * update analysis_option.yaml file
    
    * update gitignore file
    
    * update readme file
    
    * added shorebird credentials in codemagic
    
    * update codemagic build script
    
    * update codemagic build script and upgrader package translation

--- packages/common/lib/upgrader/upgrader_translation_messages.dart
+++ packages/common/lib/upgrader/upgrader_translation_messages.dart
@@ -1,25 +1,28 @@
 part of common;
 
 class UpgraderTranslationMessages extends UpgraderMessages {
+  final String code;
+  UpgraderTranslationMessages({required this.code}) : super(code: code);
+
   /// Override the message function to provide custom language localization.
   @override
   String message(UpgraderMessage messageKey) {
     if (languageCode == 'np') {
       switch (messageKey) {
         case UpgraderMessage.body:
-          return 'np {{appName}} को नयाँ संस्करण अब उपलब्ध छ। तपाईको हालको संस्करण ({{currentInstalledVersion}}) हो र पछिल्लो संस्करण ({{currentAppStoreVersion}}) हो।';
+          return '{{appName}} को नयाँ संस्करण अब उपलब्ध छ। तपाईको हालको संस्करण ({{currentInstalledVersion}}) हो र पछिल्लो संस्करण ({{currentAppStoreVersion}}) हो।';
         case UpgraderMessage.buttonTitleIgnore:
-          return 'np बेवास्ता गर्नुहोस्';
+          return 'बेवास्ता गर्नुहोस्';
         case UpgraderMessage.buttonTitleLater:
-          return 'np पछि';
+          return 'पछि';
         case UpgraderMessage.buttonTitleUpdate:
-          return 'np अहिले अपडेट गर्नुहोस्';
+          return 'अहिले अपडेट गर्नुहोस्';
         case UpgraderMessage.prompt:
-          return 'np अपडेट गर्न चाहनुहुन्छ?';
+          return 'अपडेट गर्न चाहनुहुन्छ?';
         case UpgraderMessage.releaseNotes:
-          return 'np रिलिज नोटहरू';
+          return 'रिलिज नोटहरू';
         case UpgraderMessage.title:
-          return 'np एप अपडेट गर्ने हो?';
+          return 'एप अपडेट गर्ने हो?';
       }
     }
     // Messages that are not provided above can still use the default values.
